625b653ca9930896da3560f77cb376db9b77fd20
Sebastien Langoureaux <sebastien.langoureaux@harmonie-mutuelle.fr>
2018-10-24 08:27:19 +0000

    first commit

---
+++ main.tf
@@ -0,0 +1,63 @@
+provider "aws" {
+  region = "${var.location}"
+}
+
+# Create the VPC and subnet
+module "vpc" {
+  source = "github.com/terraform-aws-modules/terraform-aws-vpc"
+
+  name                  = "${var.vpc_name}"
+  cidr                  = "${var.network}"
+  azs                   = "${var.zones}"
+  public_subnets        = ["${var.nat_subnet}"]
+  private_subnets       = "${var.subnets}"
+  enable_nat_gateway    = true
+  enable_vpn_gateway    = true
+  enable_dns_hostnames  = true
+  single_nat_gateway    = true
+
+  tags                  = "${var.tags}"
+}
+
+
+# Create security rules
+module "security_group" {
+  source = "github.com/terraform-aws-modules/terraform-aws-security-group"
+
+  name        = "admin"
+  description = "Security group for ssh, https input and all output"
+  vpc_id      = "${module.vpc.vpc_id}"
+  
+  # Allow ping between private subnets
+  ingress_cidr_blocks = "${var.subnets}"
+  ingress_rules = ["ping-icmp"]
+
+  # Allow SSH and RDP from all internal network
+  ingress_with_cidr_blocks = [
+    {
+      from_port   = 22
+      to_port     = 22
+      protocol    = "tcp"
+      description = "ssh"
+      cidr_blocks = "10.0.0.0/8"
+    },
+    {
+      from_port   = 3389
+      to_port     = 3389
+      protocol    = "tcp"
+      description = "rdp"
+      cidr_blocks = "10.0.0.0/8"
+    },
+    {
+      from_port   = 8
+      to_port     = 0
+      protocol    = "icmp"
+      description = "ping"
+      cidr_blocks = "10.0.0.0/8"
+    },
+  ]
+  
+  # Allow icmp, web, dns and ntp
+  egress_cidr_blocks = ["0.0.0.0/0"]
+  egress_rules = ["ping-icmp", "http-tcp-80", "http-tcp-443", "dns-udp-53", "ntp-udp-123"]
+}
---
+++ variables.tf
@@ -0,0 +1,25 @@
+variable "location" {
+  description = "DC area"
+}
+variable "vpc_name" {
+  description = "Ressource group name"
+}
+variable "zones" {
+  type = "list"
+  description = "A list of availability zones in the region"
+}
+
+variable "network" {
+  description = "The main network"
+}
+variable "subnets" {
+  type = "list"
+  description = "The list of subnet network"
+}
+variable "nat_subnet" {
+  description = "The nat subnet to deploy vpn/router on it"
+}
+
+variable "tags" {
+  type = "map"
+}
